bd49b62d3c63295a07aacb7c16661d942b011b09
Eliel <elielmelo.dev@gmail.com>
2023-02-01 20:38:53 -0300

    quase la

--- script.js
+++ script.js
@@ -54,4 +54,73 @@ document.querySelector(".data-mes").innerHTML = monthName;
 
 /*------------------Fim calendario-------------------------------- */
 
+/*-----------------------Scroll---------------------------- */
 
+const menuLinks = document.querySelectorAll(' .nav-list li a');
+
+function getDistanceFromTheTop(element) {
+  const id = element.getAttribute("href");
+  return document.querySelector(id).offsetTop;
+}
+
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const distanceFromTheTop = getDistanceFromTheTop(event.target) - 90;
+  smoothScrollTo(0, distanceFromTheTop);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", scrollToSection);
+});
+
+function smoothScrollTo(endX, endY, duration) {
+  const startX = window.scrollX || window.pageXOffset;
+  const startY = window.scrollY || window.pageYOffset;
+  const distanceX = endX - startX;
+  const distanceY = endY - startY;
+  const startTime = new Date().getTime();
+
+  duration = typeof duration !== "undefined" ? duration : 600;
+
+  const easeInOutQuart = (time, from, distance, duration) => {
+    if ((time /= duration / 2) < 1)
+      return (distance / 2) * time * time * time * time + from;
+    return (-distance / 2) * ((time -= 2) * time * time * time - 2) + from;
+  };
+
+  const timer = setInterval(() => {
+    const time = new Date().getTime() - startTime;
+    const newX = easeInOutQuart(time, startX, distanceX, duration);
+    const newY = easeInOutQuart(time, startY, distanceY, duration);
+    if (time >= duration) {
+      clearInterval(timer);
+    }
+    window.scroll(newX, newY);
+  }, 1000 / 60);
+}
+
+
+/*-----------------------Scroll  Fim  ---------------------------- */
+
+/*----------------------------animation scroll---------------- */
+
+
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      console.log(entry)
+      if (entry.isIntersecting){
+          entry.target.classList.add('show');
+      } else {
+          entry.target.classList.remove('show');
+      }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.ola, .calendar, .sobre, .sobre-mim, p, .habilidades, .habili-cards, .projeto-name, .projeto-cards, .contato-name, .contato');
+hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+/*----------------------------animation scroll  fim---------------- */
